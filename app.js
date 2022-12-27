0188964330be8be3ad2dc12abd002ef2e7f93050
JonBalles <jonatan.a.ballestero@gmail.com>
2022-12-27 20:04:00 -0300

    agregando html

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+app.use(express.static("public"))
+const PORT = 3000
+
+
+app.get("/", (req, res) => {
+
+    res.sendFile(path.join(__dirname, "views/home.html"))
+
+})
+
+app.listen(PORT,()=>{
+    console.log(`
+    server listen in port ${PORT}
+    http://localhost:${PORT}`)
+})
