078c7119de344583d199fcc581f146253e0d98f6
Hexanima <nicolasfilippelli22@gmail.com>
2022-12-29 12:27:27 -0300

    Agregado archivos register, ademas de estilos de prueba

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ app.get("/", (req, res) => {
 
 })
 
+app.get("/register", (req, res) => {
+
+    res.sendFile(path.join(__dirname, "views/register.html"))
+
+})
+
 app.listen(PORT,()=>{
     console.log(`
     server listen in port ${PORT}
