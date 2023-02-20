b2e8a31a593a2eead9acf41258605c34aaac18bd
RamonBritez <rbritez966@gmail.com>
2023-02-20 20:01:24 -0300

    validators add

--- app/src/controllers/productsController.js
+++ app/src/controllers/productsController.js
@@ -1,13 +1,9 @@
 const fs = require('fs');
 const path = require('path');
+const { validationResult } = require("express-validator");
+const { readJSON, writeJSON } = require("../database");
 
-const productsDB = path.join(__dirname, '../database/productsDB.json');
-
-const products = JSON.parse(fs.readFileSync(productsDB, 'utf-8'));
-
-const writeJson = (products) => {
-	fs.writeFileSync(productsDB, JSON.stringify(products), {encoding: "utf-8"})
-}
+const products = readJSON("productsDB.json");
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
@@ -37,60 +33,141 @@ const controller = {
 
 	// Create - Form to create
 	create: (req, res) => {
-		res.render("products/products-create")
+		res.render("products/products-create", {
+			products: readJSON("productsDB.json"),
+		})
 	},
 	
 	// Create -  Method to store
 	store: (req, res) => {
-		let lastId = products[products.length -1].id;
-		
-		let newProduct = {
-			id: lastId + 1,
-			name: req.body.name,
-			price: req.body.price,
-			discount: req.body.discount,
-			category: req.body.category,
-			description: req.body.description,
-			pet: req.body.pet,
-			weight: req.body.weight,
-			image: req.file ? req.file.filename : null
+		const errors = validationResult(req);
+	
+		if(req.fileValidatorError){
+		  errors.errors.push({
+			value: "",
+			msg: req.fileValidatorError,
+			param: "image",
+			location: "file",
+		  });
 		}
-		products.push(newProduct);
-
-		writeJson(products);
-
-		res.redirect(
-			"/products"
-		)
-	},
+	
+		if (!req.file) {
+		  errors.errors.push({
+			value: "",
+			msg: "El producto debe tener una imagen",
+			param: "image",
+			location: "file",
+		  });
+		}
+	
+		if (errors.isEmpty()) {
+		  const products = readJSON("productsDB.json");
+		  const { name, price, category, pet,description, discount, weight} = req.body;
+	
+		  const newProduct = {
+			id: products.length ? products[products.length - 1].id + 1 : 1,
+			name: name.trim(),
+			price: +price ,
+			discount: +discount,
+			category,
+			weight,
+			description: description.trim(),
+			pet,
+			image: req.file.filename,
+		  };
+	
+		  products.push(newProduct);
+	
+		  writeJSON("productsDB.json", products);
+	
+		  return res.redirect("/products");
+		} else {
+		  if (req.file) {
+			fs.existsSync(`./public/image/products/${req.file.filename}`) &&
+			  fs.unlinkSync(`./public/image/products/${req.file.filename}`);
+		  }
+	
+		  return res.render("products/products-create", {
+			errors: errors.mapped(),
+			old: req.body,
+			products
+		  });
+		}
+	  },
 
 	// Update - Form to edit
 	edit: (req, res) => {
+		products: readJSON("productsDB.json")
 		let productId = Number(req.params.id);
 
 		let productToEdit = products.find(product  => product.id === productId);
 
 		res.render('products/product-edit-form', {
-			productToEdit,
+			...productToEdit,
 		})
 	},
 	
 	
 	// Update - Method to update
 	update: (req, res) => {
-		let productId = Number(req.params.id);
-		products.forEach(product => {
-			if(product.id === productId){
-				product.name = req.body.name,
-				product.price = req.body.price,
-				product.discount = req.body.discount,
-				product.category = req.body.category,
-				product.description = req.body.description
+		const errors = validationResult(req);
+	
+		if(req.fileValidatorError){
+		  errors.errors.push({
+			value: "",
+			msg: req.fileValidatorError,
+			param: "image",
+			location: "file",
+		  });
+		}
+	
+		if (errors.isEmpty()) {
+		  const { name, price, category, description, discount, weight} = req.body;
+		  const products = readJSON("productsDB.json");
+	
+		  const productsModify = products.map((product) => {
+			if (product.id === +req.params.id) {
+			  let productModify = {
+				...product,
+				name: name.trim(),
+				price: +price,
+				discount: +discount,
+				category,
+				weight,
+				description: description.trim(),
+				image: req.file ? req.file.filename : product.image,
+			  };
+	
+			  if (req.file) {
+				fs.existsSync(`./public/image/products/${product.image}`) &&
+				  fs.unlinkSync(`./public/image/products/${product.image}`);
+			  }
+	
+			  return productModify;
 			}
-		});
-		writeJson(products);
-		res.redirect('/products/');
-	},
+			return product;
+		  });
+	
+		  writeJSON("productsDB.json", productsModify);
+	
+		  return res.redirect("/products");
+		} else {
+		  const products = readJSON("productsDB.json");
+	
+		  const product = products.find((product) => product.id === +req.params.id);
+	
+		  if (req.file) {
+			fs.existsSync(`./public/image/products/${req.file.filename}`) &&
+			  fs.unlinkSync(`./public/image/products/${req.file.filename}`);
+		  }
+	
+		  return res.render("products/product-edit-form", {
+			...product,
+			errors: errors.mapped(),
+			old: req.body,
+		  });
+		}
+	  },
 
 	// Delete - Delete one product from DB
 	destroy : (req, res) => {
