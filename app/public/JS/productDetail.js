653fd36833e79b70690bb3f41256ae169f3a7b49
JonBalles <jonatan.a.ballestero@gmail.com>
2023-03-08 18:25:55 -0300

    Estilos Detail ajustado. Botonera Admin. Alternar entre imagenes

--- app/public/JS/productDetail.js
+++ app/public/JS/productDetail.js
@@ -3,12 +3,9 @@
 function myFunction(imgs) {
     // Get the expanded image
     var expandImg = document.getElementById("expandedImg");
-    // Get the image text
-    var imgText = document.getElementById("imgtext");
     // Use the same src in the expanded image as the image being clicked on from the grid
     expandImg.src = imgs.src;
-    // Show the container element (hidden with CSS)
-    expandImg.parentElement.style.display = "block";
+   
   }
 
   /* FUENTE: https://www.w3schools.com/howto/howto_js_tab_img_gallery.asp */
