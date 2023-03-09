32c86d014e341cccc30b5a696ca17203f9043d27
Hexanima <nicolasfilippelli22@gmail.com>
2023-03-09 11:30:00 -0300

    Renombrado y mejorado middleware de admin a userCheck, implementado

--- app/src/routes/products.js
+++ app/src/routes/products.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../middlewares/uploadProducts')
+const {isAdmin} = require("../middlewares/userCheck")
 
 // ************ Controller Require ************
 const {index, create, store, detail, edit, update, destroy} = require('../controllers/productsController');
@@ -11,20 +12,20 @@ const productValidator = require('../validations/productValidator');
 router.get('/', index); 
 
 /*** CREATE ONE PRODUCT ***/ 
-router.get('/create', create); 
-router.post('/', upload.uploadImageProduct.array('image'), productValidator, store); 
+router.get('/create', isAdmin, create); 
+router.post('/', isAdmin, upload.uploadImageProduct.array('image'), productValidator, store); 
 
 
 /*** GET ONE PRODUCT ***/ 
 router.get('/detail/:id/', detail); 
 
 /*** EDIT ONE PRODUCT ***/ 
-router.get('/edit/:id', edit); 
-router.put('/update/:id', upload.uploadImageProduct.array('image'), productValidator, update); 
+router.get('/edit/:id', isAdmin, edit); 
+router.put('/update/:id', isAdmin, upload.uploadImageProduct.array('image'), productValidator, update); 
 
 
 /*** DELETE ONE PRODUCT***/ 
-router.delete('/delete/:id', destroy); 
+router.delete('/delete/:id', isAdmin, destroy); 
 
 
 
--- app/src/routes/users.js
+++ app/src/routes/users.js
@@ -5,24 +5,24 @@ const uploadAvatar = require("../middlewares/uploadAvatar");
 const registerValidator = require("../validations/registerValidator");
 const loginValidator = require("../validations/loginValidator");
 const editUserValidator = require("../validations/editUserValidator");
-
+const {isLogged, isVisitor} = require("../middlewares/userCheck")
 
 /* GET - Login Form */
-router.get("/login", login); 
+router.get("/login", isVisitor, login); 
 /* POST - Login user */
-router.post("/login", loginValidator, processLogin);
+router.post("/login", isVisitor, loginValidator, processLogin);
 
 /* GET - Register form */
-router.get("/register", register); 
+router.get("/register", isVisitor, register); 
 /* POST - Register user data */
-router.post("/register", uploadAvatar.single("avatar"), registerValidator, processRegister);
+router.post("/register", isVisitor, uploadAvatar.single("avatar"), registerValidator, processRegister);
 
 /* GET - User logout */
 router.get("/logout", logout)
 
 /* User Edit */
-router.get("/edit/:id", edit)
-router.put("/edit/:id", uploadAvatar.single("avatar"), editUserValidator, processEdit)
+router.get("/edit/:id", isLogged, edit)
+router.put("/edit/:id", isLogged, uploadAvatar.single("avatar"), editUserValidator, processEdit)
 
 
 module.exports = router;
