b59a71423d9a1468711db11777b46cd4efca129e
JonBalles <jonatan.a.ballestero@gmail.com>
2023-03-09 01:07:54 -0300

    Total y cantidad funcionando

--- app/public/JS/productDetail.js
+++ app/public/JS/productDetail.js
@@ -5,26 +5,33 @@ function myFunction(imgs) {
     var expandImg = document.getElementById("expandedImg");
     // Use the same src in the expanded image as the image being clicked on from the grid
     expandImg.src = imgs.src;
-   
-  }
+}
+
+
 
   /* FUENTE: https://www.w3schools.com/howto/howto_js_tab_img_gallery.asp */
 
   /* Contador de cantidad de productos */
 let counter = 1
 let $count = document.getElementById('count');
+let final = Number(document.getElementById("price").firstChild.data)
+let total = document.getElementById("totalFinal")
+  console.log(final)
 
   function add () {
     if(counter <= 10) {
         counter = counter + 1
         $count.innerHTML = `<p>${counter}</p>`
+        total.innerHTML= `<span>$${final * counter}</span>`
     }
-
+// 
   }
 
   function subtract () {
     if(counter > 0) {
         counter = counter - 1
         $count.innerHTML = `<p>${counter}</p>`
+        total.innerHTML= `<span>$${((1 + counter) * final) - final}</span>`
+        
     }
   }
