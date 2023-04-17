46d776f7754b3eb1954d2c25a32cd49ea2368286
Hexanima <nicolasfilippelli22@gmail.com>
2023-04-17 14:07:17 -0300

    Cambios a la base de datos, ejecutar script nuevo

--- app/src/database/models/Role.js
+++ app/src/database/models/Role.js
@@ -18,5 +18,13 @@ module.exports = (sequelize, dataTypes) => {
       timestamps: false,
     };
     const Role = sequelize.define(alias, cols, config);
+
+
+    Role.associate = (models) => {
+      Role.hasMany(models.User, {
+        as: "users",
+        foreignKey: "rol_id"
+      })
+    }
     return Role
   };
--- app/src/database/models/User.js
+++ app/src/database/models/User.js
@@ -1,50 +1,57 @@
-module.exports=(sequelize, dataTypes)=>{
+module.exports = (sequelize, dataTypes) => {
+  let alias = "User";
+  let cols = {
+    id: {
+      type: dataTypes.INTEGER(11).UNSIGNED,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: dataTypes.STRING(20),
+      allowNull: false,
+    },
+    last_name: {
+      type: dataTypes.STRING(20),
+      allowNull: false,
+    },
+    email: {
+      type: dataTypes.STRING(50),
+      allowNull: false,
+    },
+    avatar: {
+      type: dataTypes.STRING(100),
+      allowNull: false,
+    },
+    pass: {
+      type: dataTypes.STRING(100),
+      allowNull: false,
+    },
+    rol_id: {
+      type: dataTypes.INTEGER(11),
+      allowNull: false,
+    },
+    address_id: {
+      type: dataTypes.INTEGER(11).UNSIGNED,
+    },
+  };
+  let config = {
+    tableName: "users",
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+    deletedAt: false,
+  };
 
-    let alias="User";
-    let cols={
-        id:{
-            type:dataTypes.INTEGER(11).UNSIGNED,
-            primaryKey:true,
-            allowNull:false,
-            autoIncrement:true,
-        },
-        first_name:{
-            type:dataTypes.STRING(20),
-            allowNull:false,
-        },
-        last_name:{
-            type:dataTypes.STRING(20),
-            allowNull:false,
-        },
-        email:{
-            type:dataTypes.STRING(50),
-            allowNull:false,
-        },
-        avatar:{
-            type:dataTypes.STRING(100),
-            allowNull:false,
-        },
-        pass:{
-            type:dataTypes.STRING(100),
-            allowNull:false,
-        },
-        rol_id:{
-            type:dataTypes.INTEGER(11),
-            allowNull:false,
-        },
-        address_id: {
-             type:dataTypes.INTEGER(11).UNSIGNED,
-        },
-    }
-    let config={
-    tableName:"users",
-        timestamps:true,
-        createdAt:"created_at",
-        updatedAt:"updated_at",
-        deletedAt:false,
+  const User = sequelize.define(alias, cols, config);
 
-    }
-    const User=sequelize.define(alias,cols,config)
-    return User
+  User.associate = (models) => {
+    User.belongsTo(models.Role, {
+      as: "user_rol",
 
-}
+      foreignKey: "rol_id",
+    });
+  };
+
+  return User;
+};
