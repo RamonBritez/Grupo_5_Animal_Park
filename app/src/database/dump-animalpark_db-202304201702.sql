3162783fa6187edd567980cacda69b5572a4f258
Hexanima <nicolasfilippelli22@gmail.com>
2023-04-20 17:04:42 -0300

    Base actualizada

--- app/src/database/dump-animalpark_db-202304201702.sql
+++ app/src/database/dump-animalpark_db-202304201702.sql
@@ -19,6 +19,10 @@
 -- Table structure for table `adresses`
 --
 
+CREATE DATABASE IF NOT EXISTS animalpark_db;
+
+USE animalpark_db;
+
 DROP TABLE IF EXISTS `adresses`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -30,9 +34,10 @@ CREATE TABLE `adresses` (
   `city` varchar(50) DEFAULT NULL,
   `user_id` int(11) unsigned NOT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`),
   KEY `adresses_ibfk_1` (`user_id`),
   CONSTRAINT `adresses_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,6 +46,7 @@ CREATE TABLE `adresses` (
 
 LOCK TABLES `adresses` WRITE;
 /*!40000 ALTER TABLE `adresses` DISABLE KEYS */;
+INSERT INTO `adresses` VALUES (1,'Prueeba 123','1824','Buenos Aires','Monte Chingolo',8),(2,'Calle Falsa 123','1234','Buenos Aires','Buenos Aires',10);
 /*!40000 ALTER TABLE `adresses` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,7 +61,7 @@ CREATE TABLE `brands` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `brand` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,6 +70,7 @@ CREATE TABLE `brands` (
 
 LOCK TABLES `brands` WRITE;
 /*!40000 ALTER TABLE `brands` DISABLE KEYS */;
+INSERT INTO `brands` VALUES (1,'Voraz'),(2,'nada1'),(3,'Manualidad'),(4,'Varias img'),(5,'dfsfsdf');
 /*!40000 ALTER TABLE `brands` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,7 +91,7 @@ CREATE TABLE `cart` (
   KEY `cart_FK_1` (`product_id`),
   CONSTRAINT `cart_FK` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `cart_FK_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -116,7 +123,7 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (1,'alimentos'),(2,'accesorios'),(3,'juguetes'),(4,'vestimenta'),(5,'higiene y prevencion'),(6,'camas / cuchas');
+INSERT INTO `category` VALUES (1,'Alimentos'),(2,'Accesorios'),(3,'Juguetes'),(4,'Vestimenta'),(5,'Higiene/Prevencion'),(6,'Camas/Cuchas');
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -138,7 +145,7 @@ CREATE TABLE `orders` (
   PRIMARY KEY (`id`),
   KEY `orders_FK` (`user_id`),
   CONSTRAINT `orders_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -147,6 +154,7 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1,8,NULL,NULL,15000.00,5,0),(2,8,NULL,NULL,12.00,2,0),(3,8,NULL,NULL,6426.00,4,1);
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -170,7 +178,7 @@ CREATE TABLE `pets` (
 
 LOCK TABLES `pets` WRITE;
 /*!40000 ALTER TABLE `pets` DISABLE KEYS */;
-INSERT INTO `pets` VALUES (1,'gatos'),(2,'perros'),(3,'peces'),(4,'roedores'),(5,'aves'),(6,'reptiles');
+INSERT INTO `pets` VALUES (1,'Perros'),(2,'Gatos'),(3,'Roedores'),(4,'Peces'),(5,'Reptiles'),(6,'Aves');
 /*!40000 ALTER TABLE `pets` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -188,7 +196,7 @@ CREATE TABLE `product_images` (
   PRIMARY KEY (`id`),
   KEY `product_images_FK` (`product_id`),
   CONSTRAINT `product_images_FK` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -197,7 +205,6 @@ CREATE TABLE `product_images` (
 
 LOCK TABLES `product_images` WRITE;
 /*!40000 ALTER TABLE `product_images` DISABLE KEYS */;
-INSERT INTO `product_images` VALUES (1,'default.jpg',1),(2,'default.jpg',2),(3,'default.jpg',3),(4,'default.jpg',4),(5,'default.jpg',5),(6,'default.jpg',6),(7,'default.jpg',7),(8,'default.jpg',8),(9,'default.jpg',9),(10,'default.jpg',10),(11,'default.jpg',11),(12,'default.jpg',12),(13,'default.jpg',13),(14,'default.jpg',14),(15,'default.jpg',15),(16,'default.jpg',16),(17,'default.jpg',17),(18,'default.jpg',18),(19,'default.jpg',19),(20,'default.jpg',20),(21,'default.jpg',21),(22,'default.jpg',22),(23,'default.jpg',23),(24,'default.jpg',24),(25,'default.jpg',25),(26,'default.jpg',26),(27,'default.jpg',27),(28,'default.jpg',28);
 /*!40000 ALTER TABLE `product_images` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -215,7 +222,7 @@ CREATE TABLE `products` (
   `price` decimal(7,2) unsigned NOT NULL,
   `discount` tinyint(3) unsigned DEFAULT 0,
   `weight` decimal(4,1) unsigned DEFAULT 0.0,
-  `subcategory_id` int(11) unsigned DEFAULT NULL,
+  `category_id` int(11) unsigned DEFAULT NULL,
   `pet_id` int(11) unsigned DEFAULT NULL,
   `active` tinyint(1) unsigned NOT NULL DEFAULT 0,
   `brand_id` int(11) DEFAULT NULL,
@@ -223,12 +230,12 @@ CREATE TABLE `products` (
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `products_FK` (`pet_id`),
-  KEY `products_FK_1` (`subcategory_id`),
+  KEY `products_FK_1` (`category_id`),
   KEY `products_FK_2` (`brand_id`),
   CONSTRAINT `products_FK` FOREIGN KEY (`pet_id`) REFERENCES `pets` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT `products_FK_1` FOREIGN KEY (`subcategory_id`) REFERENCES `subcategory` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `products_FK_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `products_FK_2` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -237,7 +244,6 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,'la prueba','No se, estaba probando las validaciones y el tema de mantener la session',100.00,10,10.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(2,'Nutrique Gato Adulto Castrado','Control de peso',4105.00,0,2.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(3,'Nutrique Gato Adulto Urinary Care','Urinary Care',3689.00,0,2.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(4,'Nutrique Gato Bebé','Gato bebé y gato cachorro',11398.00,25,7.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(5,'Nutrique Gato Kitten','Kitten cachorro',782.00,0,350.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(6,'Ropa Para Perros','Buzo Vestido Ambar ',1500.00,10,0.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(7,'Nutrique Cachorro','Raza Mediana',1394.00,5,1.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(8,'Nutrique Perro Adulto','Raza pequeña y mini',7420.00,0,7.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(9,'Alfalfa en cubos','Ricos en vitaminas para todo tipo de roedores',700.00,5,100.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(10,'Shulet Tortugin','Alimento completo para tortugas acuáticas y complemento para terrestres',350.00,0,100.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(11,'Hectopar','Pulguicida para gatos mas de 4kg',500.00,0,100.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(12,'Shulet Tropical','Alimento completo balanceado para peces tropicales',870.00,0,100.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(13,'Zoomarlo','Mazorca de maíz premium',1400.00,0,300.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(14,'Sieger','Sieger katze gato urinario',3800.00,0,7.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(15,'Complete','Perro adulto +7 años',9600.00,0,20.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(16,'Alimento','Alimento para Perros Voraz Carne 21 kilos.',5200.00,10,21.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(17,'Bonacqua','Azul de mitelino 50ml',1300.00,0,50.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(18,'Alimento Whiskas','para gato adulto sabor pescado',5040.00,15,10.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(19,'Zoomarlo','asdasdaDFDASFawsdfds',1234.00,0,12.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(20,'Disfraz de Gallina','Decora a tu tortuga con este maravilloso atuendo',1500.00,0,10.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(21,'test multiple','Varias imagenes de prueba para test',8500.00,99,12.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(22,'asdasdas','dxcsdfsafdgdfsdf',333.00,30,12.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(23,'ropa tortugas','dfsdfsdfsdf',12334.00,99,10.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(24,'ropa tortugas','dfsdfsdfsdf',12334.00,99,10.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(25,'ropa tortugas','dfsdfsdfsdf',12334.00,99,10.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(26,'ropa tortugas','dfsdfsdfsdf',12334.00,99,10.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(27,'ropa tortugas','dfsdfsdfsdf',12334.00,99,10.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48'),(28,'ropa tortugas','dfsdfsdfsdf',12334.00,99,10.0,NULL,NULL,1,NULL,'2023-04-17 23:52:48','2023-04-17 23:52:48');
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -266,32 +272,6 @@ INSERT INTO `roles` VALUES (0,'User'),(1,'Admin');
 UNLOCK TABLES;
 
 --
--- Table structure for table `subcategory`
---
-
-DROP TABLE IF EXISTS `subcategory`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `subcategory` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `categories_id` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `subcategory_FK` (`categories_id`),
-  CONSTRAINT `subcategory_FK` FOREIGN KEY (`categories_id`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `subcategory`
---
-
-LOCK TABLES `subcategory` WRITE;
-/*!40000 ALTER TABLE `subcategory` DISABLE KEYS */;
-/*!40000 ALTER TABLE `subcategory` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `users`
 --
 
@@ -312,7 +292,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   KEY `users_FK` (`rol_id`),
   CONSTRAINT `users_FK` FOREIGN KEY (`rol_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -321,7 +301,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Jon','Balles','jon@mail.com','','avatar_default.jpeg',1,NULL,'2023-04-18 00:02:00','2023-04-18 00:02:00'),(2,'Jon','Balles','jon@mail.com','','avatar_default.jpeg',1,NULL,'2023-04-18 00:02:00','2023-04-18 00:02:00'),(3,'Ramón','Brietz Sanabria','test@test.com','$2a$12$7lF/l7O4CYJ2nzsqLtzX/eHDM7wCcFuF3yZJqmF72LDrzgSfNfw/m','1678210261666_avatar_.jpg',1,NULL,'2023-04-18 00:02:00','2023-04-18 00:02:00'),(4,'Jon','Balles','jon@gmail.com','$2a$12$irYdKP2o.gDv1chC/dqDaOxOlVgAbh1xe3EdQYpdUGi0.e8SybOna','11111111111111.jpg',1,NULL,'2023-04-18 00:02:00','2023-04-18 00:02:00'),(5,'test','test','test3@test.com','$2a$12$EbXyrMeBbeGwNJHeTYqowO2yFsvdpWFMfurYlMgfviI2Uvpf5b80q','1677884477039_avatar_.png',0,NULL,'2023-04-18 00:02:00','2023-04-18 00:02:00'),(6,'test','test','test3@test.com','$2a$12$EbXyrMeBbeGwNJHeTYqowO2yFsvdpWFMfurYlMgfviI2Uvpf5b80q','1677884477039_avatar_.png',0,NULL,'2023-04-18 00:02:00','2023-04-18 00:02:00'),(7,'testeando','Registro','testeo@mail.com','$2a$12$zyvgfFjvvwTfSNcoIjugN.QM2JWyh8xLHePx1/CWcAHK9lNrHmqCi','avatar_default.jpeg',0,NULL,'2023-04-18 00:02:00','2023-04-18 00:02:00'),(8,'Nico','Fili','nico@fili.com','$2a$12$dgX2HtrUhUHkagioparHjezIvbUAJ..IWqYortUsP3V3uUDuCms/G','avatar_default.jpeg',1,NULL,'2023-04-18 00:02:00','2023-04-18 00:02:00'),(9,'Cosme','Fulanito','prueba@user.com','$2a$12$KowLlmiL0detU4zCTy/RXet/xm/vsyHWYaO8iZZMQkpiaeC9xiV4y','avatar_default.jpeg',0,NULL,'2023-04-18 00:02:00','2023-04-18 00:02:00'),(10,'Admin','Fulanito','prueba@admin.com','$2a$12$9O9GmhqPlC3iySUx5EM3Ku1.8QKhXUYazyYdBCN9KgyiHRJzvsLnO','avatar_default.jpeg',1,NULL,'2023-04-18 00:02:00','2023-04-18 00:02:00');
+INSERT INTO `users` VALUES (1,'Jon','Balles','jon@mail.com','','avatar_default.jpeg',1,NULL,'2023-04-18 00:02:00','2023-04-18 00:02:00'),(2,'Jon','Balles','jon@mail.com','','avatar_default.jpeg',1,NULL,'2023-04-18 00:02:00','2023-04-18 00:02:00'),(3,'Ramón','Brietz Sanabria','test@test.com','$2a$12$7lF/l7O4CYJ2nzsqLtzX/eHDM7wCcFuF3yZJqmF72LDrzgSfNfw/m','1678210261666_avatar_.jpg',1,NULL,'2023-04-18 00:02:00','2023-04-18 00:02:00'),(4,'Jon','Balles','jon@gmail.com','$2a$12$irYdKP2o.gDv1chC/dqDaOxOlVgAbh1xe3EdQYpdUGi0.e8SybOna','11111111111111.jpg',1,NULL,'2023-04-18 00:02:00','2023-04-18 00:02:00'),(5,'test','test','test3@test.com','$2a$12$EbXyrMeBbeGwNJHeTYqowO2yFsvdpWFMfurYlMgfviI2Uvpf5b80q','1677884477039_avatar_.png',0,NULL,'2023-04-18 00:02:00','2023-04-18 00:02:00'),(6,'test','test','test3@test.com','$2a$12$EbXyrMeBbeGwNJHeTYqowO2yFsvdpWFMfurYlMgfviI2Uvpf5b80q','1677884477039_avatar_.png',0,NULL,'2023-04-18 00:02:00','2023-04-18 00:02:00'),(7,'testeando','Registro','testeo@mail.com','$2a$12$zyvgfFjvvwTfSNcoIjugN.QM2JWyh8xLHePx1/CWcAHK9lNrHmqCi','avatar_default.jpeg',0,NULL,'2023-04-18 00:02:00','2023-04-18 00:02:00'),(8,'Nico','Fili','nico@fili.com','$2a$12$dgX2HtrUhUHkagioparHjezIvbUAJ..IWqYortUsP3V3uUDuCms/G','avatar_default.jpeg',1,NULL,'2023-04-18 00:02:00','2023-04-18 00:02:00'),(9,'Cosme','Fulanito','prueba@user.com','$2a$12$KowLlmiL0detU4zCTy/RXet/xm/vsyHWYaO8iZZMQkpiaeC9xiV4y','avatar_default.jpeg',0,NULL,'2023-04-18 00:02:00','2023-04-18 00:02:00'),(10,'Admin','Fulanito','prueba@admin.com','$2a$12$9O9GmhqPlC3iySUx5EM3Ku1.8QKhXUYazyYdBCN9KgyiHRJzvsLnO','1682001320303_avatar_.png',1,0,'2023-04-18 00:02:00','2023-04-20 14:35:21'),(11,'ro','Belen','Rocio@belen.com','$2a$12$Unp1jKOojGqGh2iLy.AIIOg6Tk4VFiFmvoLZ8uuxs6X5orJvqluk2','avatar_default.jpeg',1,NULL,'2023-04-19 18:31:30','2023-04-19 18:31:30');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -338,4 +318,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-19 19:52:38
+-- Dump completed on 2023-04-20 17:02:36
