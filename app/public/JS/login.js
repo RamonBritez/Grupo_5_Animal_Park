a66575a2d17bbf9aceccdfd10319279ca0a21b06
Hexanima <nicolasfilippelli22@gmail.com>
2023-05-09 21:57:35 -0300

    Agregada validacion de selects de nuevo producto

--- app/public/JS/login.js
+++ app/public/JS/login.js
@@ -3,7 +3,6 @@ let regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
   regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/;
 const qs = (str) => document.querySelector(str);
 window.addEventListener("load", () => {
-  console.log("sueño");
   let email = qs("#email");
   let password = qs("#password");
   let ul = qs("#errores-login");
