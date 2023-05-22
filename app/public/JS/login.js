a5d1a1361b93cfaf1e256e011b225e0076ff9e2b
MoonOfDreams <rosbelg15@gmail.com>
2023-05-22 20:04:05 -0300

    areglos a la validacion del login

--- app/public/JS/login.js
+++ app/public/JS/login.js
@@ -3,22 +3,25 @@ let regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
   regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/;
 const qs = (str) => document.querySelector(str);
 window.addEventListener("load", () => {
-  console.log("sueño");
   let email = qs("#email");
   let password = qs("#password");
   let ul = qs("#errores-login");
   let errorEmail = qs("#errorEmail");
   let errorPassword = qs("#errorPassword");
-
+  let errorForm = qs("#errorForm");
   let form = qs("form");
   form.addEventListener("submit", (e) => {
     let errores = [];
-    if (!regExEmail.test(email.value)) {
-      errores.push("el email es invalido");
-    }
+    errorForm.innerText = "";
     if (!regExPass.test(password.value)) {
       errores.push("la contraseña debe tener entre 6 y 12 caracteres");
     }
+    if (email.value.length == 0 && password.value.length == 0){
+      alert("asd")
+      errorForm.innerText = "debes completar el formulario"
+    }
+
+
     if (errores.length > 0) {
       e.preventDefault();
     }
